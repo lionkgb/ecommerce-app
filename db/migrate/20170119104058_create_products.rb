345291c9fbd6c311677b69449413917b177297e1
lionkgb <lionceau.lk@gmail.com>
2017-01-19 14:51:49 +0200

    Added a new model

---
+++ db/migrate/20170119104058_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :brand
+      t.string :gender
+      t.string :color
+      t.string :style
+      t.string :movement
+      t.string :features
+      t.string :price
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end

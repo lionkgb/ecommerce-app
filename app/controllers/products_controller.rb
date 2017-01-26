2fce77683012975c83e089ac5371ea802b797897
lionkgb <lionceau.lk@gmail.com>
2017-01-26 10:49:19 +0200

    REST,model method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,49 @@
 class ProductsController < ApplicationController
+  def index 
+    @products = Product.all
+  end 
+  def show 
+    @product = Product.find_by(id: params[:id])
+  end 
+  def new
+  end 
+  def create 
+    brand = params[:brand], 
+    gender = params[:gender], 
+    color = params[:color], 
+    style = params[:style], 
+    movement = params[:movement], 
+    features = params[:features], 
+    price = params[:price], 
+    image = params[:image]
+    product = Product.new({brand: brand , gender: gender, color: color, style: style, movement: movement, features: features, price: price, image: image})
+    product.save
+    flash[:success] = "Product Created"
+    redirect_to "/products/#{product.id}"
+  end 
+  def edit 
+    @product = Product.find_by(id: params[:id])
+  end 
+  def update 
+    product = Product.find_by(id: params[:id])
+    product.assign_attributes({
+      brand: params[:brand], 
+      gender: params[:gender], 
+      color: params[:color], 
+      style: params[:style], 
+      movement: params[:movement], 
+      features: params[:features], 
+      price: params[:price], 
+      image: params[:image]
+      })
+    product.save
+    flash[:success] = "Product Updated"
+    redirect_to "/products/#{product.id}"
+  end 
+  def destroy 
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    flash[:danger] = "Product destroyed"
+    redirect_to "/products" 
+  end 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  def tax
+    price.to_i * 0.9
+  end 
+  def friendly_created_at
+    created_at_strftime("%B %e, %Y")
+  end 
 end
